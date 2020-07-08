abc011fd8b75718134847af74c97cfc8f5ba1d06
João Pedro Marques <thejoao94@gmail.com>
2020-07-07 22:09:12 -0300

    Teste - "Upload de arquivo" para o Git
    ^

---
+++ Teste git/testegit.js
@@ -0,0 +1,11 @@
+console.log("Trabalhando com atribuição de variaveis");
+const idade = 26;
+const nome = "Joao";
+const sobrenome= "Marques";
+
+// console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome e ${nome}`);
+
+nome = nome + sobrenome;
+console.log(nome);
